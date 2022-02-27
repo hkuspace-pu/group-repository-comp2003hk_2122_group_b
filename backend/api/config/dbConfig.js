71821d033e8f8c9ace9e868c91a8978ab0af836b
Star Cheuk <183@tranthinktech.com>
2022-02-27 20:59:29 +0800

    [backend] Update to use AWS

--- backend/api/config/dbConfig.js
+++ backend/api/config/dbConfig.js
@@ -13,10 +13,11 @@
 
 // Plymouth database
 const dbConfig = {
-	  user: "YCheuk",
-	  password: "QjzW109*",
-	  database: "COMP2001HK_YCheuk",
-	  server: "socem1.uopnet.plymouth.ac.uk",
+	  user: "admin",
+	  password: "Svnny4ever",
+	  database: "COMP2003HK_GROUP_B",
+	  server: "database-tree-survey.c2i34jeq5e09.us-east-1.rds.amazonaws.com",
+    port: 1433,
 	  options: {
 		    encrypt: true, // for azure
 		    trustServerCertificate: true // change to true for local dev / self-signed certs

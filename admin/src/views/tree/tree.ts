ce7fdb32d20450c8bc62730f8b393ee0240398b7
Star Cheuk <183@tranthinktech.com>
2022-03-10 22:40:49 +0800

    [admin] add function to get data from api

--- admin/src/views/tree/tree.ts
+++ admin/src/views/tree/tree.ts
@@ -1,9 +1,16 @@
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
     name: 'tree',
     data() {
         return {
         }
+    },
+    setup() {
+        console.log("starLog...... tree setup")
+        axios
+            .get("https://7ipwaamd2b.execute-api.us-east-1.amazonaws.com/test/trees")
+            .then(response => console.log('starLog http response', response))
     }
 })

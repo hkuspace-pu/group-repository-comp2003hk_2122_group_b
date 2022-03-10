599df0a131c9a64aab4c734223014fec56e2d293
Star Cheuk <183@tranthinktech.com>
2022-03-10 22:41:21 +0800

    [api] update the tree method GET

--- backend/aws-api/lambda_function.py
+++ backend/aws-api/lambda_function.py
@@ -1,6 +1,7 @@
 import sys
 import rds_config
 import pymysql
+import json
 
 # 1. Install pymysql to local directory
 # pip install -t $PWD pymysql
@@ -32,8 +33,36 @@ def lambda_handler(event, context):
     cursor.execute("SELECT * FROM trees")
 
     rows = cursor.fetchall()
+    results = []
+    for row in rows:
+        results.append({
+            "treeId": row[0],
+            "treeName": row[1],
+            "alias": row[2],
+            "scientificName": row[3],
+            "familyCode": row[4],
+            "ecologic": row[5],
+            "cap95": row[6],
+            "cap586": row[7],
+            "hkRare": row[8],
+            "cnRare": row[9],
+            "floweringStart": row[10],
+            "floweringEnd": row[11],
+            "fruitStart": row[12],
+            "fruitEnd": row[13],
+            "treeDesc": row[14]
+        })
 
     print(rows)
-    return rows
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(results)
+    }
 
 # lambda_handler('', '')

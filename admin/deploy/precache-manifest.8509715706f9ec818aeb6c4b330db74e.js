592c4db3020421a5dc25d40e45089c603a7d2b0b
Star Cheuk <183@tranthinktech.com>
2022-03-13 22:51:19 +0800

    [admin] add deployment

---
+++ admin/deploy/precache-manifest.8509715706f9ec818aeb6c4b330db74e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039b4da119bbabe10462",
+    "url": "/css/app.181b8de3.css"
+  },
+  {
+    "revision": "fba418ec461e64b31b7061df597473dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039b4da119bbabe10462",
+    "url": "/js/app.67b60d18.js"
+  },
+  {
+    "revision": "04c9b086e115351a2dc5",
+    "url": "/js/chunk-vendors.6a6d6fad.js"
+  },
+  {
+    "revision": "ad1c7122216ff6e919bd68f90e80d94d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
